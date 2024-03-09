f77fdd84f1f6686a7876b01fbc04065587c0abf1
Randula Koralage <randula@vmi1045530.contaboserver.net>
2024-03-09 09:33:43 -0600

    frontend

---
+++ frontend/fuels.config.ts
@@ -0,0 +1,12 @@
+import { createConfig } from 'fuels';
+
+export default createConfig({
+  contracts: [
+  ],
+  output: './src/contracts',
+});
+
+/**
+ * Check the docs:
+ * https://fuellabs.github.io/fuels-ts/guide/cli/config-file
+ */
